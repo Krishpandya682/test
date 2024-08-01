7d3cdcb5bfb1b1d3af07878f2a593f87ce52b4a5
Krish Pandya <kpandya@umd.edu>
2024-08-01 13:20:41 -0400

    js updates

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("test2");
-
 // Select all h2 elements
 const headings = document.querySelectorAll('h2');
 
@@ -14,7 +12,40 @@ headings.forEach(heading => {
   }
 });
 
-// Set the color of each feature widget to red
+// Function to wrap content inside a new div with the specified class name
+function wrapContentInDiv(parent, className) {
+  // Create the new div
+  const wrapperDiv = document.createElement('div');
+  wrapperDiv.className = className;
+
+  // Move all child elements of the parent into the new div
+  while (parent.firstChild) {
+    wrapperDiv.appendChild(parent.firstChild);
+  }
+
+  // Append the new div back to the parent
+  parent.appendChild(wrapperDiv);
+}
+
+// Iterate over each feature widget to add classes and wrap content
 featureWidgets.forEach(featureWidget => {
+  // Find all .row elements within the feature widget
+  const rowElements = featureWidget.querySelectorAll('.row');
+
+  rowElements.forEach(row => {
+    // Get all .col-800 elements within the current row
+    const colElements = row.querySelectorAll('.col-800');
+
+    // Check if there are at least two .col-800 elements
+    if (colElements.length >= 2) {
+      // Wrap the first .col-800 in a div with the class 'feature-widget-item-left'
+      wrapContentInDiv(colElements[0], 'feature-widget-item-left');
+
+      // Wrap the second .col-800 in a div with the class 'feature-widget-item-right'
+      wrapContentInDiv(colElements[1], 'feature-widget-item-right');
+    }
+  });
+
+  // Set the color of the feature widget to red
   featureWidget.style.color = 'red';
 });

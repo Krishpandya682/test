662ecb2f0eafd7f1bfa4ddc86afb01297802c0e2
Krish Pandya <kpandya@umd.edu>
2024-08-06 11:56:39 -0400

    js update

--- floating-sidebar.js
+++ floating-sidebar.js
@@ -3,6 +3,17 @@ window.onload = function () {
   var col8009 = document.querySelector(".col-800-9");
   var col8003 = document.querySelector(".col-800-3");
 
+  // Rename classes
+  if (col8009) {
+    col8009.classList.remove("col-800-9");
+    col8009.classList.add("col-800");
+  }
+
+  if (col8003) {
+    col8003.classList.remove("col-800-3");
+    col8003.classList.add("col-800");
+  }
+
   // Set col-800-9 to full width
   col8009.style.width = "100%";
   col8009.style.float = "left"; // Ensure it occupies the full width
